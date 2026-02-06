9f98cf1a73f4185cbaa6a21f8f3ccf9fb1906759
00glitched <conaeze3@gmail.com>
2026-02-06 20:50:18 -0300

    load: copy to pico

--- load.sh
+++ load.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-local_PATH=pwd
-WHO=whoami
-cp $local_PATH/build/pico_os.uf2 /media/$WHO/RPI-RP2/pico_os.uf2
+LPATH=$(pwd)
+WHO=$(whoami)
+cp $LPATH/build/pico_os.uf2 /media/$WHO/RPI-RP2/pico_os.uf2
